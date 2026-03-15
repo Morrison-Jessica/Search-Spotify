6136ee8dc1cc400449a524a95a8cc9344cc49985
Jessica Morrison <jlmorrison@student.fullsail.edu>
2026-03-15 15:21:04 -0600

    Google OAuth set up and working. Login redirects to google consent screen, callback also working. Bug-fixed a leading space in authURL

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,7 +7,16 @@ const GOOGLE_USERINFO_URL = "https://www.googleapis.com/oauth2/v2/userinfo";
 // url search params...
 // http://localhost:3000/api/v1/auth/login
 const login = async (req, res) => {
-    res.status(200).json('message From AUTH API');
+    const params = new URLSearchParams({
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+        response_type: 'code',
+        scope: 'profile email',
+        access_type: 'offline',
+        prompt: 'consent'
+    });
+    const authURL = `${GOOGLE_AUTH_URL}?${params.toString()}`;
+    res.redirect(authURL);
     
 };
 
