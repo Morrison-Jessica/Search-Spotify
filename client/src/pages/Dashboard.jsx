ae0bc02b4fe9ac690ff40d0c44eadb21d0fd8207
Jessica Morrison <jlmorrison@student.fullsail.edu>
2026-03-29 19:46:11 -0600

    Frontend setup started to connect to backend server. Added redirect route to app home page, and setup appRoutes.js to recieve controller functions.

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,26 @@
+// 📄 🏠 Dashboard - 📋 📐 📚
+import Form from '../components/form';
+import Chart from '../components/chart';
+import TradeList from '../components/tradeList';
+// 🔃 
+import { useState } from 'react';
+
+// =======================================
+// 🏠 Form, Chart, TradeList 
+const Dashboard = () => {
+  // // 🔃 - 📋
+  // const [,] = useState();
+  // // 🔃 - 📐
+  // const [,] = useState();
+  // // 🔃 - 📚
+  // const [,] = useState();
+
+  return ( 
+    <>
+    <h1>Dashboard</h1>
+    </>
+  );
+
+  //return (...);
+} // end Dashboard
+export default Dashboard;
